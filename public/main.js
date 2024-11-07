00fc1894d26687a072da791f257addeecc4b120b
Ariel-Joe <arieljoe@gmail.com>
2024-11-07 21:53:21 +0300

    version 01

---
+++ public/main.js
@@ -0,0 +1,30 @@
+const addToDo = (event) => {
+    event.preventDefault();
+
+    const userInput = document.getElementById('Input').value;
+
+    const taskContainer = document.createElement('div');
+    taskContainer.style.cssText = 'display: flex; flex-direction: row; align-Items: center';
+    
+    const newPara = document.createElement('p');
+    newPara.style.cssText = 'padding: 5px 10px; min-width: 300px; margin-top: 10px';
+    newPara.textContent = userInput;
+    
+    const delBtn = document.createElement('button');
+    delBtn.textContent = 'Delete';
+    delBtn.style.cssText = 'background-color: red; padding: 5px 10px; color: white; margin-top: 10px';
+    delBtn.addEventListener('click', () => {
+        taskContainer.remove()
+    })
+    
+    taskContainer.appendChild(newPara);
+    taskContainer.appendChild(delBtn);
+    
+    const taskDisplay = document.getElementById('toDoList');
+    taskDisplay.appendChild(taskContainer);
+    
+
+    const toDoList = [];
+    toDoList.push(userInput);  
+    console.log(toDoList);  
+};
